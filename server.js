c05227e7e54016dc66add90391db1b978b47c088
avenje <jeremyavenarius@gmail.com>
2017-11-26 18:24:51 -0700

    heroku additions

--- server.js
+++ server.js
@@ -9,21 +9,16 @@ var cheerio= require("cheerio");
 var app = express();
 var port = process.env.PORT || 3000;
 
-// set up handlebars engine
 app.engine('handlebars', expressHandlebars({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
-// Serve static content for the app from the 'public' directory in the
-// application directory.
 app.use(express.static(__dirname + '/public'));
 
-// Override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
 app.use(bodyParser.json());
-// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: false
 }));
